7c5b964a9f174346484279026767198db345c778
Alwil17 <willialfred24@gmail.com>
2025-05-23 15:39:36 +0000

    Add AWS credentials to DynamoDB client and implement get_conversation_messages method for retrieving messages by conversation ID

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import logging
 from typing import List
 
 import boto3
+from boto3.dynamodb.conditions import Key
 
 from src.config import env_vars
 ## Simple edit
@@ -58,8 +59,32 @@ class Utils:
 
     @staticmethod
     def insert_data(item):
-        dynamo_client = boto3.client("dynamodb", region_name=env_vars.AWS_REGION_NAME)
+        dynamo_client = boto3.client(
+            "dynamodb",
+            region_name=env_vars.AWS_REGION_NAME,
+            aws_access_key_id=env_vars.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=env_vars.AWS_SECRET_ACCESS_KEY
+        )
         dynamo_client.put_item(
             TableName=env_vars.DYNAMO_TABLE,
             Item=item,
         )
+
+    @staticmethod
+    def get_conversation_messages(conversation_id: str):
+        """Récupère tous les messages d'une conversation spécifique"""
+        dynamo_resource = boto3.resource(
+            "dynamodb",
+            region_name=env_vars.AWS_REGION_NAME,
+            aws_access_key_id=env_vars.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=env_vars.AWS_SECRET_ACCESS_KEY
+        )
+        table = dynamo_resource.Table(env_vars.DYNAMO_TABLE)
+        
+        response = table.query(
+            IndexName="conversation_id-timestamp-index",  # Vous devrez créer cet index dans DynamoDB
+            KeyConditionExpression=Key("conversation_id").eq(conversation_id),
+            ScanIndexForward=True  # Trier par timestamp croissant
+        )
+        
+        return response.get("Items", [])
